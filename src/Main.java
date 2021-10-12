d8f565fc44e1f96672075ea7ad7a64cb5749cdd5
Ella Frost <ded_19@hotmail.com>
2021-10-12 10:26:00 +0200

    New pizzas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Pizza margarita = new Pizza ("Margarita", 57, 1,new String[]{"Tomatoes", "Cheese"});
+        Pizza vesuvio = new Pizza ("Vesuvio", 57, 2, new String[]{"Tomatoes", "Cheese", "Ham", "Oregano"});
+        Pizza hawaii = new Pizza ("Hawaii", 60, 3, new String[]{"Tomatoes", "Cheese", "Ham", "Pineapple", "Oregano"});
 
 
         Order oneOneTwo = new Order(new Pizza[]{margarita, margarita},"Lasse","112", "7:30");
